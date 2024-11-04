b56542a340291b995791fcae2995d14895d2cfbe
sunil4ul <sunil4ul5017@gmail.com>
2024-11-04 16:33:21 +0545

    add like , comment , tweet, and playlist models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,10 +168,18 @@ const logOutUser = asyncHandler (async (req,res) =>{
 
     await User.findByIdAndUpdate(
         req.user._id,
+        // {
+        //     $set:{
+        //         refreshToken: undefined
+        //     }
+        // }
         {
-            $set:{
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
+        },
+        {
+            new: true
         }
     )
 
